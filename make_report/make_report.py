abfd654c25d6700e10a0114ae2990ba5ce6653d7
uttank <uttank@snu.ac.kr>
2020-08-04 17:20:24 +0900

    first

--- make_report/make_report.py
+++ make_report/make_report.py
@@ -1,4 +1,6 @@
 import win32com.client as win32
+import win32gui
+import win32con
 import os
 
 hwp=win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
@@ -8,10 +10,11 @@ current_file = current_dir + '\\daily_report.hwp'
 hwp.Open(current_file)
 
 field_list = [ i for i in hwp.GetFieldList().split('\x02')]
-field_replace = {"working_day":"2020-08-04","check_server":"서버점검: 이상없음","check_mail":"메일 확인"}
+field_replace = {"working_day":"2020-07-04","check_server":"서버점검: 점검중","check_mail":"메일 확인"}
 
 for field in field_list :
     hwp.PutFieldText(f'{field}{{{{0}}}}',field_replace[field])
 
+hwp.SaveAs(current_dir + '\\daily_report_new.hwp')
 hwp.Quit()
-option.IgnoreMessage = 1
+
